701a65bbc4b7683665844758b857c882bd42751c
Laurids <realjupi1001@gmail.com>
2026-03-01 14:02:48 +0100

    Add tooltips for character stats in CharacterBlock component
    - Introduced new tooltip functionality for character stats (Qi, Attack, Defense, Vitality) to provide detailed breakdowns on hover.
    - Created a new utility file for calculating and formatting stat breakdowns, enhancing the clarity of character information.
    - Updated CSS to style tooltips and ensure proper visibility and interaction.

--- src/components/CharacterBlock/CharacterBlock.tsx
+++ src/components/CharacterBlock/CharacterBlock.tsx
@@ -3,11 +3,22 @@ import { useSelector } from "react-redux";
 import { RootState } from "../../state/store";
 import { formatRealm } from "../../constants/realmProgression";
 import { getEffectiveCombatStats } from "../../state/selectors/characterSelectors";
+import {
+  getAttackBreakdown,
+  getDefenseBreakdown,
+  getHealthBreakdown,
+  getQiBreakdown,
+  formatStatBreakdown,
+} from "./characterBlockUtils";
 import "./CharacterBlock.css";
 
 export const CharacterBlock = () => {
   const character = useSelector((state: RootState) => state.character);
   const combat = useSelector(getEffectiveCombatStats);
+  const attackBreakdown = useSelector(getAttackBreakdown);
+  const defenseBreakdown = useSelector(getDefenseBreakdown);
+  const healthBreakdown = useSelector(getHealthBreakdown);
+  const qiBreakdown = useSelector(getQiBreakdown);
 
   return (
     <div className="app__characterBlock">
@@ -17,10 +28,30 @@ export const CharacterBlock = () => {
       )}
       <div className="app__characterBlock-items">
         <ul className="app__characterBlock-ul">
-          <li>Qi: {Math.round(character.qi * 100) / 100}</li>
-          <li>Attack: {combat.attack}</li>
-          <li>Defense: {combat.defense}</li>
-          <li>Vitality: {character.currentHealth}/{combat.health}</li>
+          <li className="app__characterBlock-stat">
+            <span>Qi: {Math.round(character.qi * 100) / 100}</span>
+            <div className="app__characterBlock-tooltip" role="tooltip">
+              {qiBreakdown}
+            </div>
+          </li>
+          <li className="app__characterBlock-stat">
+            <span>Attack: {combat.attack}</span>
+            <div className="app__characterBlock-tooltip" role="tooltip">
+              {formatStatBreakdown(attackBreakdown, "Attack")}
+            </div>
+          </li>
+          <li className="app__characterBlock-stat">
+            <span>Defense: {combat.defense}</span>
+            <div className="app__characterBlock-tooltip" role="tooltip">
+              {formatStatBreakdown(defenseBreakdown, "Defense")}
+            </div>
+          </li>
+          <li className="app__characterBlock-stat">
+            <span>Vitality: {character.currentHealth}/{combat.health}</span>
+            <div className="app__characterBlock-tooltip" role="tooltip">
+              {formatStatBreakdown(healthBreakdown, "Vitality")}
+            </div>
+          </li>
           <li>Spirit Stones: {character.money}</li>
         </ul>
       </div>
